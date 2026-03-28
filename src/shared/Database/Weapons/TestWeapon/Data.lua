15dee86b5e6dd221cb90199d4c74f99690e701a0
Kunru Li <kunrul0406@gmail.com>
2026-03-27 21:46:41 -0700

    Bunch of stuff that happened next

--- src/shared/Database/Weapons/TestWeapon/Data.lua
+++ src/shared/Database/Weapons/TestWeapon/Data.lua
@@ -1,20 +1,26 @@
 return {
 	["Name"] = "Test Sword",
+	["Type"] = "Sword",
 	["Family"] = "Sword",
 	["Rarity"] = "COMMON",
 
 	["BaseDamage"] = 10,
 	["__Stats"] = {
-		-- Empty, but stats would go here
+		["Strength"] = 2,
+		["PhysicalAttack"] = 4,
+		["CritDamage"] = 0.2,
 	},
+	["Weight"] = 4,
 	["AttackContext"] = {
 		["MaxDashRange"] = 10,
 		["SwingRange"] = 6,
 	},
+	["WeaponPassive"] = nil,
 
-	-- No need for Animations, as those
-	-- are linked to the "Family"
-	["RightClickAbility"] = nil, --function() end,
+	-- No need for animations, as those
+	-- are linked to the weapon family/type.
+	["WeaponAbility"] = nil,
+	["RightClickAbility"] = nil,
 
 	["Desc"] = "SWORD_TEST_DESC",
 }
