cef1df0f6d2fa468ffc59f20ef5f02fc0960e404
Kunru Li <kunrul0406@gmail.com>
2026-04-13 16:31:14 -0700

    new build, some cool imports

---
+++ src/shared/Database/Weapons/Damaged/Greatsword/Data.lua
@@ -0,0 +1,19 @@
+return {
+	["Name"] = "Crude Iron Greatsword",
+	["Type"] = "Claymore",
+	["Family"] = "Damaged",
+	["Rarity"] = "CRUDE",
+
+	["BaseDamage"] = 28,
+	["__Stats"] = {
+		["Strength"] = 8,
+		["Defense"] = 6,
+	},
+	["Weight"] = 40,
+	["WeaponPassive"] = nil,
+
+	["WeaponAbility"] = nil,
+	["RightClickAbility"] = nil,
+
+	["Desc"] = "A battered greatsword with enough weight left to split armor.",
+}
